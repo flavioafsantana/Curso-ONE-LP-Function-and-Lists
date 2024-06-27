d6f6463b7639798a201137e5ec08a76bf6111269
flavioafsantana <flavioafsantana@gmail.com>
2024-06-26 22:05:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function consoleClick() {
+    console.log('O botão foi clicado');
+}
+
+function alertClick() {
+    alert('Eu amo JS');
+}
+
+function promptClick() {
+    let cidade = prompt('Qual a sua cidade?');
+    alert('Estive em ' + cidade + ' e lembrei de você.');
+}
+
+function somaClick() {
+    let n1 = prompt('Digite um número');
+    let n2 = prompt('Digite outro número');
+    let resultado = Number(n1) + Number(n2);
+    alert(`${n1} + ${n2} = ${resultado}`);
+}
